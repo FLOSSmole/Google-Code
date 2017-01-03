e43c62d42b33663d98705e6a91eb50ab94237ad1
Megan Squire <megansquire@gmail.com>
2017-01-03 15:40:00 -0500

    Update gcProjectDates.py
    minor bug fixes and reformatting for syr server

--- gcProjectDates.py
+++ gcProjectDates.py
@@ -1,47 +1,55 @@
-import MySQLdb
-import datetime
+# -*- coding: utf-8 -*-
+"""
+(c) megan squire for FLOSSmole
+GPL v3 license applies
+"""
+import pymysql
+from datetime import datetime as dt
 import codecs
 
 linenum = 1
 datasource_id = 58459
 
-db = MySQLdb.connect(host="", 
-                    user="",
-                    passwd="",
-                    db="",
-                    use_unicode=True,
-                    charset = "utf8")
+db = pymysql.connect(host="",
+                     user="",
+                     passwd="",
+                     db="google_code",
+                     use_unicode=True,
+                     charset="utf8",
+                     autocommit=True)
 cursor = db.cursor()
 
-lines= codecs.open("gcProjectDates.txt", 'r', encoding='utf-8', errors='ignore')
-next (lines)
+lines = codecs.open('gcProjectDates.txt',
+                    'r',
+                    encoding='utf-8',
+                    errors='ignore')
+next(lines)
 for line in lines:
-    print "current line is:", linenum
+    #print(linenum)
     currentLine = line.split(',')
-    print(currentLine[0])
-    
-    if len(currentLine) == 6: 
+    print("working on:", currentLine[0])
+
+    if len(currentLine) == 6:
         try:
             proj_name = currentLine[0]
             timestamp = currentLine[4]
-            print timestamp
             if timestamp != "[NULL]":
-                convertDate = datetime.datetime.fromtimestamp(float(timestamp)).strftime('%Y-%m-%d %H:%M:%S')
-            
+                convertDate = dt.fromtimestamp(float(timestamp)).strftime('%Y-%m-%d %H:%M:%S')
+
                 cursor.execute("INSERT INTO gc_project_creates(\
                 proj_name, \
                 datasource_id, \
-                timestamp, \
-                convert_date, \
+                created, \
+                convert_created, \
                 last_updated) \
                 VALUES (%s,%s,%s,%s,%s)",
-                (proj_name, 
-                datasource_id, 
-                timestamp, 
-                convertDate, 
-                datetime.datetime.now(),))
-                db.commit()
-        except MySQLdb.Error as error:
+                (proj_name,
+                 datasource_id,
+                 timestamp,
+                 convertDate,
+                 dt.now()))
+
+        except pymysql.Error as error:
             print(error)
             db.rollback()
     linenum = linenum + 1
